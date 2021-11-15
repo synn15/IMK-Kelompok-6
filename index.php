bc57690de37f22633530bf457cf91a0c78331c5c
Ab_Azmi <azmi.kiva@gmail.com>
2021-11-15 14:32:37 +0700

    Bikin navbar

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- <link rel="stylesheet" href="assets/bootstrap.min.css"> -->
+</head>
+<body>
+    <?php include 'views/navbar.php'; ?>
+    <main>
+        <h1 class="h1">Hello Neighbor</h1>
+        <h1 class="bg-primary">Tes bootsrtarp 5 hallo bimooo</h1>
+    </main>
+</body>
+</html>
---
+++ views/navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar">
+    <div class="navbar-main">
+        <div class="navbar-main__logo">
+            <h1>A</h1>
+        </div>
+        <div class="navbar-main__search">
+            <div class="navbar-main__search-container">
+                <input type="text" name="search" id="" placeholder="search...">
+                <button class="btn btn-search">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+        <div class="navbar-main__icons">
+            <a href="#" class="logout">Logout</a>
+            <a href="#" class="notif">
+                <i class="far fa-bell icon"></i>
+            </a>
+            <a href="#" class="keranjang">
+                <i class="fas fa-cart-plus icon"></i>
+            </a>
+            <a href="#" class="profil">
+                <i class="far fa-user-circle icon"></i>
+            </a>
+        </div>
+    </div>
+    <div class="navbar-sub">
+        <div class="navbar-sub__links">
+            <a href="#" class="navbar-sub__link active">Home</a>
+            <a href="#" class="navbar-sub__link">Kategori</a>
+            <a href="#" class="navbar-sub__link">Toko Saya</a>
+            <a href="#" class="navbar-sub__link">Menyewa</a>
+            <a href="#" class="navbar-sub__link">Kembalikan</a>
+        </div>
+    </div>
+</nav>
