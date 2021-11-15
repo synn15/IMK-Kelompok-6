232ea5d7ce54ceb6eddc9bbe7f894595c0e592c4
Ab_Azmi <azmi.kiva@gmail.com>
2021-11-15 23:47:28 +0700

    Nambah Banner Statis

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,11 +13,68 @@
     <link rel="stylesheet" href="css/main.css">
     <!-- <link rel="stylesheet" href="assets/bootstrap.min.css"> -->
 </head>
+
 <body>
     <?php include 'views/navbar.php'; ?>
-    <main>
-        <h1 class="h1">Hello Neighbor</h1>
-        <h1 class="bg-primary">Tes bootsrtarp 5 hallo bimooo</h1>
-    </main>
+    <section class="banner">
+        <div class="banner__big">
+            <div id="slideshow" class="slideshow">
+                <div class="slide__container sld-1">
+                    <div class="slide__detail">
+                        <h1 class="slide__detail-title">PLAYSTATION 5</h1>
+                        <p class="slide__detail-description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Sed purus mi, feugiat sit amet pharetra eu, molestie porttitor odio. Aliquam a mi libero.
+                            Proin congue viverra lorem, in scelerisque leo vehicula quis.
+                            Integer et eros eu libero ullamcorper tempor.
+                        </p>
+                        <div class="slide__detail-stars">
+                            <div class="star star-full"></div>
+                            <div class="star star-full"></div>
+                            <div class="star star-full"></div>
+                            <div class="star"></div>
+                            <div class="star"></div>
+                        </div>
+                        <button class="slide__detail-button btn">Sewa Sekarang</button>
+                    </div>
+                    <div class="slide__labels">
+                        <span class="barang__label">Trending</span>
+                        <span class="barang__label">Kualitas Terjamin</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="banner__medium">
+            <div class="banner__medium-labels">
+                <div class="barang__label">Kualitas Terjamin</div>
+            </div>
+            <div class="banner__medium-detail">
+                <div class="banner__medium-detail__title">
+                    Gaun Kawin
+                </div>
+                <div class="banner__medium-detail__subtitle">
+                    Rias Pengantin Nina
+                </div>
+            </div>
+        </div>
+        <div class="banner__small">
+            <div class="barang__label">HOT</div>
+        </div>
+    </section>
+    <section class="kategori">
+        <h1 class="kategori__judul">Kategori</h1>
+        <div class="kategori__pills">
+            <a class="kategori__pill">
+                <div class="kategori__pils-icon">
+
+                </div>
+
+            </a>
+        </div>
+    </section>
 </body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- <script src="js/banner.js"></script> -->
+
 </html>
