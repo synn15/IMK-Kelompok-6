e14284c3ecdb19003b76e028e2352c17e8fcb664
Ab_Azmi <azmi.kiva@gmail.com>
2021-12-04 14:23:57 +0700

    Bikin halaman Keranjang

---
+++ views/keranjang.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../php/header.php'; ?>
+
+<body>
+    <?php include '../php/navbar.php'; ?>
+
+    <section class="keranjang-container">
+        <div class="judul">
+            <h1 class="judul-title">Keranjang</h1>
+        </div>
+        <div class="keranjang-list mt-5">
+            <div class="keranjang-product">
+                <div class="kiri">
+                    <div class="keranjang-img">
+                        <img src="https://media.gq.com/photos/56e71c0b14cbe0637b261d7f/4:3/w_2260,h_1695,c_limit/horseinsuit2.jpg" alt="" srcset="">
+                    </div>
+                    <div class="keranjang-detail">
+                        <div class="keranjang-title">
+                            Kuda Sarjana
+                        </div>
+                        <p class="keranjang-deskripsi">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium alias, facilis eligendi consequuntur fuga quia aspernatur quam et nihil, eveniet officiis facere, aperiam explicabo! Distinctio laboriosam nobis laudantium illo dolorum!
+                        </p>
+                        <div class="keranjang-rating-toko">
+                            Toko Dina
+                            <div class="keranjang-bintang">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="kanan">
+                    <div class="keranjang-jumlah">
+                        <button type="button">-</button>
+                        12
+                        <button type="button">+</button>
+                    </div>
+                    <div class="keranjang-togel">
+                        <label class="switch switch200">
+                            <input type="checkbox">
+                            <span class="slider slider200"></span>
+                        </label>
+                        Tambahkan
+                    </div>
+                </div>
+            </div>
+            <div class="keranjang-product">
+                <div class="kiri">
+                    <div class="keranjang-img">
+                        <img src="https://i.etsystatic.com/26585955/r/il/a768ed/2793982007/il_fullxfull.2793982007_kual.jpg" alt="" srcset="">
+                    </div>
+                    <div class="keranjang-detail">
+                        <div class="keranjang-title">
+                            Maid Outfit
+                        </div>
+                        <p class="keranjang-deskripsi">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium alias, facilis eligendi consequuntur fuga quia aspernatur quam et nihil, eveniet officiis facere, aperiam explicabo! Distinctio laboriosam nobis laudantium illo dolorum!
+                        </p>
+                        <div class="keranjang-rating-toko">
+                            Toko Dina
+                            <div class="keranjang-bintang">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="kanan">
+                    <div class="keranjang-jumlah">
+                        <button type="button">-</button>
+                        12
+                        <button type="button">+</button>
+                    </div>
+                    <div class="keranjang-togel">
+                        <label class="switch switch200">
+                            <input type="checkbox">
+                            <span class="slider slider200"></span>
+                        </label>
+                        Tambahkan
+                    </div>
+                </div>
+            </div>
+            <div class="keranjang-product">
+                <div class="kiri">
+                    <div class="keranjang-img">
+                        <img src="https://cdn11.bigcommerce.com/s-i32t4keptf/images/stencil/1280x1280/products/3038/3544/all3__63339.1626199785.jpg?c=1" alt="" srcset="">
+                    </div>
+                    <div class="keranjang-detail">
+                        <div class="keranjang-title">
+                            NERF47 Mainan Anak
+                        </div>
+                        <p class="keranjang-deskripsi">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium alias, facilis eligendi consequuntur fuga quia aspernatur quam et nihil, eveniet officiis facere, aperiam explicabo! Distinctio laboriosam nobis laudantium illo dolorum!
+                        </p>
+                        <div class="keranjang-rating-toko">
+                            Toko Dina
+                            <div class="keranjang-bintang">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="kanan">
+                    <div class="keranjang-jumlah">
+                        <button type="button">-</button>
+                        12
+                        <button type="button">+</button>
+                    </div>
+                    <div class="keranjang-togel">
+                        <label class="switch switch200">
+                            <input type="checkbox">
+                            <span class="slider slider200"></span>
+                        </label>
+                        Tambahkan
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="keranjang-btn mt-5">
+            <div class="keranjang-total">
+                Rp.2.000.000
+            </div>
+            <a href="/imk-kelompok-6/views/pembayaran.php" class="btn-bayar">
+                <button type="submit">
+                    <i class="fas fa-money-check-alt"></i>
+                    Bayar
+                </button>
+            </a>
+        </div>
+
+    </section>
+
+</body>
+<?php include '../php/footer.php'; ?>
+
+</html>
