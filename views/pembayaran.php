06e34cf7f1d94e59d0f4e77db1c0732ca99bc89d
Ab_Azmi <azmi.kiva@gmail.com>
2021-12-06 15:19:18 +0700

    Pembayaran page

---
+++ views/pembayaran.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../php/header.php'; ?>
+
+<body>
+    <?php include '../php/navbar.php'; ?>
+
+
+    <section class="tabs">
+        <div class="tile" id="tile-1">
+
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs nav-justified" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="alamat-tab" data-bs-toggle="tab" data-bs-target="#alamat" type="button" role="tab" aria-controls="alamat" aria-selected="true">
+                        <i class="fas fa-map-marked-alt"></i>
+                        Alamat
+                    </button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pengiriman-tab" data-bs-toggle="tab" data-bs-target="#pengiriman" type="button" role="tab" aria-controls="pengiriman" aria-selected="false">
+                        <i class="fas fa-truck-moving"></i>
+                        Pengiriman
+                    </button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pembayaran-tab" data-bs-toggle="tab" data-bs-target="#pembayaran" type="button" role="tab" aria-controls="pembayaran" aria-selected="false">
+                        <i class="far fa-credit-card"></i>
+                        Pembayaran
+                    </button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="checkout-tab" data-bs-toggle="tab" data-bs-target="#checkout" type="button" role="tab" aria-controls="checkout" aria-selected="false">
+                        <i class="far fa-handshake"></i>
+                        Checkout
+                    </button>
+                </li>
+            </ul>
+
+            <!-- Tab panes -->
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="alamat" role="tabpanel" aria-labelledby="alamat-tab">
+                    <form action="">
+                        <div class="row mt-5">
+                            <div class="col-6">
+                                <div class="input-group input-group-lg flex-nowrap">
+                                    <span class="input-group-text" id="addon-wrapping">Nama Lengkap</span>
+                                    <input type="text" class="form-control" placeholder="John Doe" aria-label="Username" aria-describedby="addon-wrapping">
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="input-group input-group-lg flex-nowrap">
+                                    <span class="input-group-text" id="addon-wrapping">No HP</span>
+                                    <input type="text" class="form-control" placeholder="0813xxxx" aria-label="Username" aria-describedby="addon-wrapping">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col-9">
+                                <div class="input-group input-group-lg">
+                                    <span class="input-group-text">Alamat<br>Lengkap</span>
+                                    <textarea class="form-control" rows="6" aria-label="With textarea"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col-3 offset-6">
+                                <div class="input-group input-group-lg flex-nowrap">
+                                    <span class="input-group-text" id="addon-wrapping">Kode Post</span>
+                                    <input type="text" class="form-control" placeholder="5676xxx" aria-label="Username" aria-describedby="addon-wrapping">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="tab-pane fade" id="pengiriman" role="tabpanel" aria-labelledby="pengiriman-tab">
+                    <form action="">
+                        <div class="row mt-5">
+                            <div class="col-6">
+                                <div class="input-group input-group-lg mb-3">
+                                    <label class="input-group-text" for="inputGroupSelect01">Layanan<br>Pengiriman</label>
+                                    <select class="form-select" id="inputGroupSelect01">
+                                        <option selected>JNE</option>
+                                        <option value="1">J&T</option>
+                                        <option value="2">SiCepat</option>
+                                        <option value="3">Grab</option>
+                                        <option value="3">Pos Indonesia</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="input-group input-group-lg mb-3">
+                                    <label class="input-group-text" for="inputGroupSelect01">Waktu Antar</label>
+                                    <select class="form-select" id="inputGroupSelect01">
+                                        <option selected>Jam Kerja</option>
+                                        <option value="1">Bebas</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <h3>Biaya : Rp. 50.000</h3>
+                        </div>
+                    </form>
+
+                </div>
+                <div class="tab-pane fade" id="pembayaran" role="tabpanel" aria-labelledby="pembayaran-tab">
+                    <div class="row mt-5">
+                        <div class="col-4">
+                            <div class="metode-pembayaran">
+                                <div class="credit-card active">
+                                    <div class="credit-title">
+                                        <h4>Credit Card</h4>
+                                        <p>Direct Payment</p>
+                                    </div>
+                                    <i class="fab fa-cc-visa"></i>
+                                </div>
+                                <div class="transfer">
+                                    <div class="credit-title">
+                                        <h4>Bank Transfer</h4>
+                                        <p>3-4 Work Days</p>
+                                    </div>
+                                </div>
+                                <div class="paypal">
+                                    <div class="credit-title">
+                                        <h4>PayPal</h4>
+                                        <p>Direct Payment</p>
+                                    </div>
+                                    <i class="fab fa-cc-paypal"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="input-group input-group-lg mb-3">
+                                <span class="input-group-text">CC</span>
+                                <input type="text" class="form-control" placeholder="Credit Number" aria-label="Credit Number">
+                                <input type="text" class="form-control" placeholder="CVC" aria-label="CVC">
+                            </div>
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                                <label class="form-check-label" for="flexSwitchCheckDefault">
+                                    Agree with the <a href="#" style="text-decoration: none;">Terms and Conditions</a>
+                                </label>
+                            </div>
+                            <button class="btn-checkout mt-5">Pay</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="checkout" role="tabpanel" aria-labelledby="checkout-tab">
+                    <div class="checkout">
+                        <div class="checkout-header">
+                            <h2>
+                                Penyewaan Berhasil
+                            </h2>
+                            <p>13:20, Senin 17 Agustus 2021</p>
+                        </div>
+                        <hr>
+                        <div class="checkout-body">
+                            <div class="row"></div>
+                        </div>
+                        <div class="checkout-footer">
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+</body>
+<?php include '../php/footer.php'; ?>
+
+<script>
+
+</script>
+
+</html>
