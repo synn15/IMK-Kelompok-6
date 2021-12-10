e4522870f13881f2fefdeabcc0f9be98b0444d81
Ab_Azmi <azmi.kiva@gmail.com>
2021-12-10 15:10:12 +0700

    Benerin logo navbar

--- php/navbar.php
+++ php/navbar.php
@@ -1,30 +1,34 @@
 <nav class="custom-navbar">
-    
-    <div class="navbar-main">
-        <div class="navbar-main__logo">
+
+    <div class="navbar-wrapper">
+        <div class="navbar-logo">
             <img class="logo-img" src="/imk-kelompok-6/assets/img/logosewain.png" alt="" srcset="">
         </div>
-        <div class="navbar-main__search">
-            <div class="navbar-main__search-container">
-                <input type="text" name="search" id="" placeholder="search...">
-                <button class="btn btn-search">
-                    <i class="fas fa-search"></i>
-                </button>
+        <div class="navbar-main">
+            <div class="navbar-main__search">
+                <div class="navbar-main__search-container">
+                    <input type="text" name="search" id="" placeholder="search...">
+                    <button class="btn btn-search">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="navbar-main__icons">
+                <a href="/imk-kelompok-6/views/login.php" class="logout">Login</a>
+                <a href="#" class="notif">
+                    <i class="far fa-bell icon"></i>
+                </a>
+                <a href="/imk-kelompok-6/views/keranjang.php" class="keranjang">
+                    <i class="fas fa-cart-plus icon"></i>
+                </a>
+                <a href="/imk-kelompok-6/views/profil.php" class="profil">
+                    <i class="far fa-user-circle icon"></i>
+                </a>
             </div>
         </div>
-        <div class="navbar-main__icons">
-            <a href="/imk-kelompok-6/views/login.php" class="logout">Login</a>
-            <a href="#" class="notif">
-                <i class="far fa-bell icon"></i>
-            </a>
-            <a href="/imk-kelompok-6/views/keranjang.php" class="keranjang">
-                <i class="fas fa-cart-plus icon"></i>
-            </a>
-            <a href="/imk-kelompok-6/views/profil.php" class="profil">
-                <i class="far fa-user-circle icon"></i>
-            </a>
-        </div>
+
     </div>
+
     <div class="navbar-sub">
         <div class="navbar-sub__links">
             <a href="/imk-kelompok-6/index.php" class="navbar-sub__link active">Home</a>
